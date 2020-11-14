e0725dc66bd248ca6d5d570b119c05cd6647b404
Thomas Roberts <robertsorange@gmail.com>
2020-11-14 11:21:43 -0700

    Keeping player in bounds

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -33,6 +33,12 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Keep within bounds of the screen
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.05f, 0.95f);
+        pos.y = Mathf.Clamp(pos.y, 0.05f, 0.95f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
         if (Input.GetKey(up))
             vely = SPEED;
         else if (Input.GetKey(down))

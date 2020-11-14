0e8d1ce5d3b672292610adcef76a23d6a39cfa34
Vincent Levy <vicentelevy.a@gmail.com>
2020-11-13 21:00:23 -0700

    made a basic level selector; quit button done babyyyy

---
+++ Assets/LevelProperties.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProperties : MonoBehaviour
+{
+    public GameObject Gift;
+}
---
+++ Assets/LevelSelector/SelectLevel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    //////////////////////IMPORTANT////////////////////////////
+    //All these lists must have the SAME length
+    public Transform[] LevelLocations = new Transform[2];
+    public string[] LevelNames = new string[2];
+    public GameObject SelectorSprite;
+    int CurrentSelection;
+    private GameObject Selector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets the selector to the first place in the list
+        CurrentSelection = 0;
+        Selector = Instantiate(SelectorSprite, LevelLocations[CurrentSelection]);
+        ChangeSelection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player selects a level then draws the sprite at that location
+        //if the player presses up or down the selection changes
+        //if the player presses enter selects the level and the level starts
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            CurrentSelection++;
+            if (CurrentSelection > LevelLocations.Length - 1)
+                CurrentSelection = 0;
+
+            ChangeSelection();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            CurrentSelection--;
+            if (CurrentSelection < 0)
+                CurrentSelection = LevelLocations.Length - 1;
+
+             ChangeSelection();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+             LevelSelect();
+        }
+    }
+
+
+    private void LevelSelect()
+    {
+        //transition into the scene
+        //get name from the string array
+        Debug.Log(LevelNames[CurrentSelection]);
+        //SceneManager.LoadScene(LevelNames[CurrentSelection]);
+    }
+
+    private void ChangeSelection()
+    {
+        //throw new NotImplementedException();
+
+        //puts the sprite over the selected level
+        Selector.transform.position = LevelLocations[CurrentSelection].position;
+        //SelectorLocations[CurrentSelection].position;
+    }
+
+}

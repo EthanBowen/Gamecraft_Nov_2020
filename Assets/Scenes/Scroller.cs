0a2b8f6053daf2f2cc4008dca7f22cf5664c28ed
Thomas Roberts <robertsorange@gmail.com>
2020-11-14 11:50:20 -0700

    Camera moving, with player alongside it

---
+++ Assets/Scenes/Scroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lerpedPosition = Vector2.right * speed * Time.deltaTime;
+        transform.position += new Vector3(lerpedPosition.x, lerpedPosition.y, 0);
+    }
+}

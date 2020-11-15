dbb019483b0a97cdf67f7355a6dd123991fc44a6
Vincent Levy <vicentelevy.a@gmail.com>
2020-11-14 23:34:07 -0700

    added menu button on the menu, my head hurts btw

---
+++ Assets/MenuButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonHandler : MonoBehaviour
+{
+    public GameObject CreditsMenu;
+    public GameObject InstructionsMenu;
+
+    public void OnCredits()
+    {
+        
+        InstructionsMenu.SetActive(false);
+        CreditsMenu.SetActive(!CreditsMenu.activeSelf);
+    }
+
+    public void OnInstructions()
+    {
+        CreditsMenu.SetActive(false);
+        InstructionsMenu.SetActive(!InstructionsMenu.activeSelf);
+    }
+}

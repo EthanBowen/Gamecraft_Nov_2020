d6b6a1eb7670538484476d78a815a1f02fb0a43d
Thomas Roberts <robertsorange@gmail.com>
2020-11-15 11:59:14 -0700

    Fixed issues with credit page not left-aligned Updated fonts

--- Assets/MenuButtonHandler.cs
+++ Assets/MenuButtonHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuButtonHandler : MonoBehaviour
 {
@@ -9,7 +7,6 @@ public class MenuButtonHandler : MonoBehaviour
 
     public void OnCredits()
     {
-        
         InstructionsMenu.SetActive(false);
         CreditsMenu.SetActive(!CreditsMenu.activeSelf);
     }

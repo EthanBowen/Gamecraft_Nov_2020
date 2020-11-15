94e894c2532691c33fd6a495e45e90630a8c0d5e
James Youngblood <james@youngbloods.org>
2020-11-15 12:58:54 -0700

    Add Turkey and Candy Skull enemies

--- Assets/Scripts/CandySkullAI.cs
+++ Assets/Scripts/CandySkullAI.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class DummyController : MonoBehaviour
+public class CandySkullAI : MonoBehaviour
 {
-    public int HP = 3;
+    public int HP = 2;
     public float speed = 2f;
+    public GameObject shot;
+    public Transform shootPoint;
 
     private Transform player;
     private bool wasActive;
 
+    private Vector2 firingPoint;
+    private bool cooldown;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        firingPoint = new Vector2(Random.Range(5f, 10f), Random.Range(-5f, 5f));
     }
 
 
@@ -25,7 +31,7 @@ public class DummyController : MonoBehaviour
             if(!wasActive)
                 gameObject.AddComponent<Scroller>();
             wasActive = true;
-            MoveTowardsPlayer();
+            PrepareAttack();
         }
         else
         {
@@ -45,13 +51,32 @@ public class DummyController : MonoBehaviour
         return point.x >= 0 && point.x <= 1 && point.y >= 0 && point.y <= 1;
     }
 
-    private void MoveTowardsPlayer()
+    private void PrepareAttack()
     {
-        // Passed the player, continue forward
-        if ((transform.position - player.position).x < 0)
-            transform.position += transform.right * speed * Time.deltaTime;
-        else
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        if (!cooldown)
+        {
+            Vector3 target = player.position;
+            target.x += firingPoint.x;
+            target.y += firingPoint.y;
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+            //Stop moving, start shooting
+            if (Vector2.Distance(transform.position, target) < 1)
+            {
+                cooldown = true;
+                StartCoroutine(Shoot());
+            }
+                
+        }
+    }
+
+    private IEnumerator Shoot() {
+        Instantiate(shot, shootPoint.position, shootPoint.rotation);
+
+        yield return new WaitForSeconds(1);
+
+        firingPoint = new Vector2(Random.Range(5f, 10f), Random.Range(-5f, 5f));
+        cooldown = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
